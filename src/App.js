547b1ed84fc7f2597c9145322d389e44363a61c7
Pedrin <pedro515vieira@gmail.com>
2022-03-10 16:27:28 -0300

    :heavy_check_mark: feat: Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return(
       
-    </div>
-  );
+    )
+  };
 }
 
 export default App;
