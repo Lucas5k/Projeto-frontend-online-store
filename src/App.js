75773afd6dd1a624bbd6dd903e6c41fcaeaf4f1f
Jovane Castro <jovanecastro12@gmail.com>
2022-03-10 16:55:29 -0300

    :bug: fix: ESLint tamanho de linha.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
       </div>
     );
   }
