859f55d9936782c760fa1215f209154cc07ae807
Jovane Castro <jovanecastro12@gmail.com>
2022-03-10 23:40:52 -0300

    :sparkles: feat: Initial CSS

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
