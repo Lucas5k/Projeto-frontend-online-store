55d77f07ca366d441a76fc6b09b1bf746894dd62
Pedrin <pedro515vieira@gmail.com>
2022-03-15 15:16:58 -0300

    :heavy_check_mark: feat: Requisito 13

--- src/components/CartBtn.js
+++ src/components/CartBtn.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class CartBtn extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.handleCart();
+  }
+
+  handleCart = () => {
+    const cartItems = localStorage.getItem('cartProducts');
+    this.setState({ cartCount: JSON.parse(cartItems).length });
+  }
+
   render() {
+    const { cartCount } = this.state;
     return (
       <Link data-testid="shopping-cart-button" to="/shoppingcart" className="cartLink">
         <div>
           <img src="https://cdn-icons-png.flaticon.com/512/34/34627.png" alt="Cart icon." />
+          <span data-testid="shopping-cart-size">{ cartCount }</span>
         </div>
       </Link>
     );
